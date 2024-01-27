53c195134a8e9628ca048303e7490a675e208d47
SwasthK <220981@sdmcujire.in>
2024-01-27 11:46:27 +0530

    UPDATE frontend

--- frontend/src/compo/Card.jsx
+++
@@ -1,6 +0,0 @@
-// export default function Card({ children }) {
-//     return <div className="bg-slate-100 p-8 border-2 border-black m-7 rounded-lg">
-//       {children}
-//     </div>
-//   }
-  
